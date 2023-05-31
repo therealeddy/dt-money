3e6e219b43d698d42bdf8f1d9285144511b8a83f
therealeddy <eddy.developerr@gmail.com>
2023-05-31 08:14:04 -0300

    feat: search form component

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { MagnifyingGlass } from 'phosphor-react'
+
+import { SearchFormContainer } from './styles'
+
+const SearchForm: React.FC = () => {
+  return (
+    <SearchFormContainer>
+      <input type="text" placeholder="Busque por transações" />
+
+      <button type="submit">
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
+
+export default SearchForm
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
+
 import { Header, Summary } from '../../components'
+
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+import SearchForm from './components/SearchForm'
 
 const Transactions: React.FC = () => {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Tipo da transacao</td>
+              <td>
+                <PriceHighlight variant="income">10</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>Data</td>
+            </tr>
+            <tr>
+              <td>Tipo da transacao 2</td>
+              <td>
+                <PriceHighlight variant="outcome">- 10</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>Data</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
